5328c52de328b7e3a5cbe3f0fcbfcbbd3b8223bd
DENDUlean <denis.stoklas@seznam.cz>
2023-11-27 23:15:15 +0100

    Vytvoření funkce pro tvorbu dlaždic a doplnění logiky

--- JS/app.js
+++ JS/app.js
@@ -12,7 +12,14 @@ let awaitingEndOfMove = false;
 
 // tvoření funkce pro vytvoření naše dlaždice
 function buildSingleGrid(color){
+    // vytváříme div ve proměnné
     const element = document.createElement('div');
+    // přidáváme vytvořenému divu classu pomocí add() metodě
+    element.classList.add('single-grid');
+    // nastavujeme vytvořennému elementu atribut s náhodnou barvu
+    element.setAttribute("data-color", color);
+
+    return element;
 }
 
 // Logika pro vybíraní náhodných barev
@@ -21,6 +28,10 @@ for(let i = 0; i < gridCount; i++){
     const randomIndex = Math.floor(Math.random() *colorsPickList.length);
     // zde ukládáme barvu na náhodnem indexu, který byl vygenerován výšše
     const color = colorsPickList[randomIndex];
+    // vytváříme dlaždici s barvou
+    const grid = buildSingleGrid(color);
     // odstraňujeme barvu z pole
     colorsPickList.splice(randomIndex, 1);
+    // připojujeme nově vytvořennou dlaždici do grid containeru 
+    gridContainer.appendChild(grid);
 }
