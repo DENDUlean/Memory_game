36d8e8dff1ddd627b82f82e41d22b7d8af18fc17
DENDUlean <denis.stoklas@seznam.cz>
2023-11-27 16:00:43 +0100

    Tvorba logiky pro výběr náhodné barvy

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,18 @@ const gridCount = colorsPickList.length;
 let revealedCount = 0;
 let activeGrid = null;
 let awaitingEndOfMove = false;
+
+// tvoření funkce pro vytvoření naše dlaždice
+function buildSingleGrid(color){
+    const element = document.createElement('div');
+}
+
+// Logika pro vybíraní náhodných barev
+for(let i = 0; i < gridCount; i++){
+    // zde vytváříme generátor náhodné barvy podle délky našeho pole barev
+    const randomIndex = Math.floor(Math.random() *colorsPickList.length);
+    // zde ukládáme barvu na náhodnem indexu, který byl vygenerován výšše
+    const color = colorsPickList[randomIndex];
+    // odstraňujeme barvu z pole
+    colorsPickList.splice(randomIndex, 1);
+}
