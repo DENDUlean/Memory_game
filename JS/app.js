cb859fa7797ed9e1988132c8c47ac39962e7ecd2
DENDUlean <denis.stoklas@seznam.cz>
2023-11-27 15:50:57 +0100

    Tvorba proměnných ohledně stavu naší hry

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,11 @@
+// základní proměnné
 const gridContainer = document.querySelector('.grid-container');
 const listOfColors = ['blue', 'red', 'yellow', 'purple', 'gold', 'brown', 'aqua', 'teal'];
 const colorsPickList = [...listOfColors, ...listOfColors];
 const gridCount = colorsPickList.length;
+
+// Status naší hry
+// nastavuje nám kolik budeme mít zobrazených karet
+let revealedCount = 0;
+let activeGrid = null;
+let awaitingEndOfMove = false;
