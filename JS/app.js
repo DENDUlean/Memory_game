31dcb9babb06cb494ca50d92ec7d2ef79de382e3
DENDUlean <denis.stoklas@seznam.cz>
2023-11-27 15:46:29 +0100

    Tvorba základních proměnných (např. duplicita barev, etc...)

---
+++ JS/app.js
@@ -0,0 +1,4 @@
+const gridContainer = document.querySelector('.grid-container');
+const listOfColors = ['blue', 'red', 'yellow', 'purple', 'gold', 'brown', 'aqua', 'teal'];
+const colorsPickList = [...listOfColors, ...listOfColors];
+const gridCount = colorsPickList.length;
